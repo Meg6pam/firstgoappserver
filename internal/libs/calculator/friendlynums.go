8be2dd964f5dec3386c18f2d9868d5b47e11af3a
Ilya Peshkin <it.nor@yandex.ru>
2020-02-28 10:07:59 +0300

    В файле lib/calculator/friendlynumsv2 реализован новый алгоритм поиска дружественных чисел. В целях повышения скорости убрал логирование.

--- internal/libs/calculator/friendlynums.go
+++ internal/libs/calculator/friendlynums.go
@@ -29,15 +29,18 @@ func GetAmicableNumber(input string) (string, bool, error) {
 	case inputAsNumber == -1:
 		{
 			t0 = time.Now()
-			fmt.Printf("Elapsed time: %v", t1.Sub(t0))
+		}
+	case inputAsNumber == -3:
+		{
 			t1 = time.Now()
+			fmt.Printf("Elapsed time: %v", t1.Sub(t0))
 		}
 	case err != nil:
 		inputAsNumber = -2
 	case inputAsNumber <= 0:
 		log.Fatal(errors.New("number should be greater than 0"))
-	case inputAsNumber > math.MaxInt32:
-		log.Fatal(errors.New("number should be lesser than " + strconv.Itoa(math.MaxInt32)))
+	case inputAsNumber > math.MaxInt64:
+		log.Fatal(errors.New("number should be lesser than " + strconv.Itoa(math.MaxInt64)))
 	}
 
 	possibleFriendlyNumber := sumOfDividers(int(inputAsNumber))
