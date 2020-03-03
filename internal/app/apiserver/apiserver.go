78ba0a0b1952fd45b47951f6d86921e2c59eb065
Ilya Peshkin <it.nor@yandex.ru>
2020-03-03 14:12:20 +0300

    Роутер fasthttp. Вторая Попытка реализовать http-сервер на буферизованных каналах. Неудачная. Проблема в том, что RequestCTX в канал входит целый, а из канала вылезает без полей UserValue и URI

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,10 +5,11 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
 	"http-rest-api/internal/libs/calculator"
+	"strings"
 )
 
 var (
-	requestChan chan *fasthttp.RequestCtx
+	requestChan chan fasthttp.RequestCtx
 	donechan    chan string
 )
 
@@ -19,10 +20,6 @@ type APIServer struct {
 }
 
 func New(config *Config) *APIServer {
-	/*	for i := 0; i < 16; i++ { // threads
-		go newWorker()
-	}*/
-	go newWorker()
 	return &APIServer{
 		serverconfig: config,
 		logger:       logrus.New(),
@@ -31,14 +28,15 @@ func New(config *Config) *APIServer {
 }
 
 func (s *APIServer) Start() error {
-
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
 
 	s.configureLogger()
 	s.configureRouter()
-
+	for i := 0; i < 16; i++ { // threads
+		go newWorker()
+	}
 	s.logger.Info("starting API server...")
 
 	return fasthttp.ListenAndServe(s.serverconfig.BinAddr, s.router.Handler)
@@ -55,21 +53,36 @@ func (s *APIServer) configureLogger() error {
 }
 
 func (s *APIServer) configureRouter() {
-	requestChan = make(chan *fasthttp.RequestCtx, 1000000)
+	requestChan = make(chan fasthttp.RequestCtx, 1000000)
 	s.router.GET("/:id", saveRequest)
 }
 
 func saveRequest(ctx *fasthttp.RequestCtx) {
-	go func() { requestChan <- ctx }()
+	go func() { requestChan <- *ctx }()
 }
 
-func handleRequest(ctx *fasthttp.RequestCtx) {
-	input := ctx.UserValue("id").(string)
+func handleRequest(ctx fasthttp.RequestCtx) {
+	logrus.Info("entering in handleRequest")
+	var input string
+	if ctx.UserValue("id") != nil {
+		input = ctx.UserValue("id").(string)
+	} else {
+		input = strings.TrimLeft("/", string(ctx.Request.URI().PathOriginal()))
+	}
+	in := strings.TrimLeft("/", string(ctx.Request.Header.RequestURI()))
+	logrus.Info("in + " + in)
+	inp := strings.TrimLeft("/", string(ctx.Request.URI().Path()))
+	logrus.Info("inp " + inp)
+	//fmt.Fprintf(ctx, "Hello, %s!\n", ctx.UserValue("id"))
+	logrus.Info("got input" + input)
 	output, _, err := calculator.GetAmicableNumberv2(input)
+	logrus.Info("got output")
 	if err != nil {
 		output = "Error in handleRequest function"
 	}
-	ctx.WriteString(output)
+	//ctx.WriteString(output)
+	ctx.Response.SetBodyString(output)
+	logrus.Info("Sent response" + output)
 	//	s.logger.Info("Got number " + input["id"] + ". Sent response " + output)
 }
 
